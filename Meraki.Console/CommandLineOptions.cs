92f4cb1891481796264db00acf3125a1755cb780
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-09-20 20:55:27 +1000

    Add test and console project

---
+++ Meraki.Console/CommandLineOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace Meraki.Console
+{
+    class CommandLineOptions
+    {
+        [Option('k', "apiKey", HelpText = "Meraki API Key", MetaValue = "API_KEY")]
+        public string ApiKey { get; set; }
+    }
+}
---
+++ Meraki.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandLine;
+using Meraki;
+
+namespace Meraki.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Parser.Default.ParseArguments<CommandLineOptions>(args)
+                    .WithParsed<CommandLineOptions>(clo => Test(clo.ApiKey).Wait());
+            }
+            catch (Exception ex)
+            {
+                System.Console.Error.WriteLine(ex);
+            }
+        }
+
+        static async Task Test(string apiKey)
+        {
+            MerakiClient merakiClient = MerakiClientFactory.Create(mcs => mcs.Key = apiKey);
+
+            IReadOnlyList<Organization> organizations = await merakiClient.GetOrganizationsAsync();
+            System.Console.WriteLine(string.Join(", ", organizations.Select(org => $"{org.Name} ({org.Id})")));
+        }
+    }
+}
