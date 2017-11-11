a4a7b61dde9c0e49279187e4eed772ed5e933a1f
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-11-11 19:33:32 +1100

    Create IApiClient and HttpApliClient
    First step to make calling API calls mockable.

---
+++ src/Meraki/HttpApiClient.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Meraki
+{
+    /// <summary>
+    /// Call HTTP apis.
+    /// </summary>
+    internal class HttpApiClient
+    {
+        private readonly HttpClient _client;
+        private readonly UrlFormatProvider _formatter = new UrlFormatProvider();
+
+        /// <summary>
+        /// Create a new <see cref="HttpApiClient"/>.
+        /// </summary>
+        /// <param name="baseAddress">
+        /// The base URI for web service calls. This must be absolute and cannot be null.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="settings"/> cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// The <see cref="MerakiDashboardClientSettings.Address"/> or <see cref="MerakiDashboardClientSettings.Key"/> fields
+        /// cannot be null, empty or whitespace.
+        /// </exception>
+        public HttpApiClient(Uri baseAddress, HttpRequestHeaders httpRequestHeaders = null)
+        {
+            if (baseAddress == null)
+            {
+                throw new ArgumentNullException(nameof(baseAddress));
+            }
+            if (!baseAddress.IsAbsoluteUri)
+            {
+                throw new ArgumentException("Must be absolute URI", nameof(baseAddress));
+            }
+
+            _client = new HttpClient(new HttpClientHandler())
+            {
+                BaseAddress = new Uri(baseAddress.AbsoluteUri, UriKind.Absolute)
+            };
+            if (httpRequestHeaders != null)
+            {
+                foreach (KeyValuePair<string, IEnumerable<string>> header in httpRequestHeaders)
+                {
+                    _client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Format and escape the given string for a URI.
+        /// </summary>
+        /// <param name="formattable">
+        /// The string to format.
+        /// </param>
+        /// <returns>
+        /// The formatted string.
+        /// </returns>
+        public string Url(FormattableString formattable)
+        {
+            return formattable.ToString(_formatter);
+        }
+
+        /// <summary>
+        /// Call the given URL asynchronously.
+        /// </summary>
+        /// <param name="method">
+        /// </param>
+        /// <param name="uri">
+        /// The URI to call.
+        /// </param>
+        /// <returns>
+        /// The <see cref="HttpRequestMessage"/>.
+        /// </returns>
+        /// <exception cref="HttpRequestException">
+        /// The request failed.
+        /// </exception>
+        public async Task<HttpResponseMessage> SendAsync(HttpMethod method, string uri)
+        {
+            using (HttpResponseMessage response = await _client.SendAsync(new HttpRequestMessage(method, uri)))
+            {
+                response.EnsureSuccessStatusCode();
+                return response;
+            }
+        }
+
+        /// <summary>
+        /// Call the given URL asynchronously using an HTTP GET method, expecting returned data.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The expected return type.
+        /// </typeparam>
+        /// <param name="uri">
+        /// The URI to call.
+        /// </param>
+        /// <returns>
+        /// The result as a string.
+        /// </returns>
+        /// <exception cref="HttpRequestException">
+        /// The request failed.
+        /// </exception>
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            return JsonConvert.DeserializeObject<T>(await GetAsync(uri));
+        }
+
+        /// <summary>
+        /// Call the given URL asynchronously using an HTTP GET method, expecting returned data.
+        /// </summary>
+        /// <param name="uri">
+        /// The URI to call.
+        /// </param>
+        /// <returns>
+        /// The result as a string.
+        /// </returns>
+        /// <exception cref="HttpRequestException">
+        /// The request failed.
+        /// </exception>
+        public async Task<string> GetAsync(string uri)
+        {
+            using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri))
+            using (HttpResponseMessage response = await _client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
