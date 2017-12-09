745fc2bb02eb178d1af43823049bb9f4723d2fb2
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-12-09 22:37:07 +1100

    Split out UrlFormatter and add tests

--- src/Meraki.Dashboard/UrlFormatProvider.cs
+++ src/Meraki.Dashboard/UrlFormatProvider.cs
@@ -8,33 +8,16 @@ namespace Meraki.Dashboard
     /// </summary>
     internal class UrlFormatProvider : IFormatProvider
     {
-        private readonly UrlFormatter _formatter = new UrlFormatter();
+        private static readonly UrlFormatter Formatter = new UrlFormatter();
 
         public object GetFormat(Type formatType)
         {
             if (formatType == typeof(ICustomFormatter))
             {
-                return _formatter;
+                return Formatter;
             }
 
             return null;
-        }
-
-        private class UrlFormatter : ICustomFormatter
-        {
-            public string Format(string format, object arg, IFormatProvider formatProvider)
-            {
-                if (arg == null)
-                {
-                    return string.Empty;
-                }
-                if (format == "r")
-                {
-                    return arg.ToString();
-                }
-
-                return Uri.EscapeDataString(arg.ToString());
-            }
         }
     }
 }
